ebc7d674f8b4e530f2567326dde6628cc85cf382
SchmidtyBM <82740541+SchmidtyBM@users.noreply.github.com>
2024-03-30 19:15:40 -0600

    Update questionCreation.php

--- A5/questionCreation.php
+++ A5/questionCreation.php
@@ -1,3 +1,39 @@
+<?php
+    session_start();
+    require_once("db.php");
+
+    if (!isset($_SESSION["user_id"])){
+        header("Location: index.php");
+        exit();
+    } 
+    if (isset($_POST["submit"]) && $_POST["submit"]){
+
+        $user_id = $_SESSION["user_id"];
+
+        try {
+            $db = new PDO($attr, $db_user, $db_pwd, $options);
+        } 
+        catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
+
+        $question = $_POST["question"];
+
+        if (strlen($question) > 0 && strlen($question) <= 1500){
+            $query = "INSERT INTO Questions (user_id, question, created_dt) VALUES ('$user_id', '$question', NOW())";
+            $result = $db->exec($query);
+
+            $db = null;
+            header("Location: questionManagement.php");
+            exit();
+        }
+        else{
+            $error = ("Question must be non-empty and less than 1500 characters");
+        }
+    }
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,14 +56,14 @@
         </main>
 
         <main id="main-center">
-            <form id="creation-form" class="auth-form" action="questionDetail.php" method="post">
+            <form id="creation-form" class="auth-form" method="post">
                 <p>
                     <label for="text-area">Ask Your Question:</label>
                     <textarea id="text-area" name="question" rows="10" cols="5" placeholder="Enter Your Question"></textarea>
                     <p id="charcount" class="charcount">0/1500</p>
                 </p>
                 <p>
-                    <input type="submit" value="Post Question"/>
+                    <input type="submit" value="Post Question" name="submit"/>
                 </p>
             </form>
         </main>
